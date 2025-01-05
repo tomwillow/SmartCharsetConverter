ce56fc078c75e1789fe8448b8b83c11413f13db2
tomwillow <tomwillow@qq.com>
2025-01-06 00:10:05 +0800

    [resource] add resource

---
+++ src/SmartCharsetConverter-imgui/resource.h
@@ -0,0 +1,60 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 SmartCharsetConverter.rc 使用
+//
+#define IDD_DIALOG_MAIN                 101
+#define IDI_ICON1                       102
+#define IDR_MENU_RIGHT                  106
+#define IDR_MENU_SELECT_LANGUAGES       107
+#define IDR_LANGUAGEJSON_ENGLISH        108
+#define IDR_LANGUAGEJSON2               109
+#define IDR_LANGUAGEJSON_SIMPLIFIED_CHINESE 109
+#define IDR_LANGUAGEJSON1               111
+#define IDR_LANGUAGEJSON_SPANISH        111
+#define IDC_RADIO_STRETEGY_SMART        1001
+#define IDC_RADIO_STRETEGY_MANUAL       1002
+#define IDC_RADIO_STRETEGY_NO_FILTER    1003
+#define IDC_EDIT_INCLUDE_TEXT           1004
+#define IDC_BUTTON_ADD_DIR              1010
+#define IDC_BUTTON_ADD_FILES            1011
+#define IDC_RADIO_TO_ORIGIN             1020
+#define IDC_RADIO_TO_DIR                1021
+#define IDC_EDIT_OUTPUT_DIR             1022
+#define IDC_BUTTON_SET_OUTPUT_DIR       1023
+#define IDC_RADIO_UTF8                  1030
+#define IDC_RADIO_UTF8BOM               1031
+#define IDC_RADIO_GB18030               1032
+#define IDC_RADIO_OTHER                 1033
+#define IDC_COMBO_OTHER_CHARSET         1034
+#define IDC_LISTVIEW                    1040
+#define IDC_BUTTON_START                1050
+#define IDC_BUTTON_CLEAR                1051
+#define IDC_CHECK_CONVERT_RETURN        1052
+#define IDC_RADIO_CRLF                  1053
+#define IDC_RADIO_LF                    1054
+#define IDC_RADIO_CR                    1055
+#define IDC_SYSLINK1                    1057
+#define IDC_BUTTON1                     1058
+#define IDC_BUTTON_SETTINGS             1058
+#define IDC_STATIC_FILE_LISTS           1059
+#define IDC_STATIC_SET_FILTER_MODE      1060
+#define IDC_STATIC_ADD_FILES_OR_FOLDER  1061
+#define IDC_STATIC_SET_OUTPUT           1062
+#define IDC_STATIC_SET_OUTPUT_CHARSET   1063
+#define ID_OPEN_WITH_NOTEPAD            40003
+#define ID_REMOVE_ITEM                  40004
+#define ID_40005                        40005
+#define ID_SPECIFY_ORIGIN_CHARSET       40006
+#define ID_40007                        40007
+#define ID_LANGUAGE                     40008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        112
+#define _APS_NEXT_COMMAND_VALUE         40009
+#define _APS_NEXT_CONTROL_VALUE         1064
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
